fc4ac6b2c76230436b40d22f990ed830adc1086d
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-02-04 20:45:17 -0600

    setup clear DB

---
+++ config/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HOST: "us-cdbr-iron-east-04.cleardb.net",
+    USER: "b0a591d44dad18",
+    PASSWORD: "de6ac666",
+    DB: "heroku_5940a70023a72a6"
+  };
+
+  //DATABASE_URL: mysql://b0a591d44dad18:de6ac666@us-cdbr-iron-east-04.cleardb.net/heroku_5940a70023a72a6?reconnect=true
