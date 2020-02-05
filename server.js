c59edf693f284d27a7a843ee62b2b8fa64599ef1
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-02-04 20:53:24 -0600

    clean up server file

--- server.js
+++ server.js
@@ -2,25 +2,6 @@ const inquirer = require ('inquirer');
 const mysql = require("mysql");
 const connection = require('./config/connection');
 
-/*
-const connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-
-    user: "root",
-
-    password: "",
-    database: "emp_tracker"
-  });
-  
-  connection.connect((err) => {
-    if (err) throw err;
-     console.log("connected as id " + connection.threadId + "\n");
-     awaitInfo();
-  });
-*/
-
   function promptUser() {
     return inquirer
       .prompt([
